0c3f9d8f153b5d3c4e3ca8f8d0c772487a2a5e04
bizayush26-cell <biz.ayush26@gmail.com>
2026-06-06 09:39:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+import cron from "node-cron";
+import { main } from "./src/utils/main.js";
+
+// Schedule a task to run every 30 mins
+cron.schedule("*/30 * * * *", async () => {
+  const currentTime = new Date().toLocaleTimeString();
+  console.log(`Running a task every 30 mins - ${currentTime}`);
+  await main();
+});
+
+console.log("ES Module Cron job server started...");
+
+// import { main } from "./src/utils/main.js";
+// import express from "express";
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.get("/sendpoll", async (req, res) => {
+//   try {
+//     await main();
+//     res.send("Polls sent successfully!");
+//   } catch (error) {
+//     console.error("Error in main function:", error);
+//     res.status(500).send("An error occurred while sending polls.");
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+// pm2 start app.js --name "PrepSOTGBot" --watch
